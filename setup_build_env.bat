81b423e3d16932d27b54d3fc6efdac9d8679c466
kasicass <kasicass@gmail.com>
2020-04-30 19:09:37 +0800

    env

---
+++ setup_build_env.bat
@@ -0,0 +1,8 @@
+
+:: set PATH & INCLUDE
+set C99_DIR=%~dp0\c99\
+set PATH=%C99_DIR%;%C99_DIR%c99toc89\;%PATH%
+set INCLUDE=%C99_DIR%msinttypes\;%INCLUDE%
+
+:: open msys.bat
+:: D:\MinGW\msys\1.0\msys.bat
